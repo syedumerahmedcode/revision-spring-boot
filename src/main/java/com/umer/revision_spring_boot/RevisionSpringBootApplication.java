0c5efc25773f9ca649e59b87190a09c7f490c3c3
umer <syedumerahmedcode@gmail.com>
2025-12-28 12:13:15 +0100

    first commit

---
+++ src/main/java/com/umer/revision_spring_boot/RevisionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.umer.revision_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umer/revision_spring_boot/RevisionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umer.revision_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
